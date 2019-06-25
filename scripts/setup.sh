cf1234bec44c4d834218ef9731911d9b36273614
Jugnu <jugnu@appveen.com>
2019-06-25 11:26:17 +0530

    build script updated

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd $WORKSPACE
-
+echo "****************************************************"
+echo "govault :: Fetching dependencies"
+echo "****************************************************"
 go get -u github.com/boltdb/bolt
 go get -u github.com/gorilla/mux
